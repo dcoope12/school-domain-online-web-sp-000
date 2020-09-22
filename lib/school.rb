1146381d858d6ac6c98afd9c35607c477fedd75d
Devin Cooper <dcoope12@asu.edu>
2020-09-22 00:46:01 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,8 +1,30 @@
 class School
 
-def initialize(name)
-  @name = name
+def initialize(school_name)
+  @school_name = name
+  roster = {}
 end
 
+def roster
+  @roster
+end
 
+def add_student(name, grade)
+  @student_name = name
+  @grade = grade
+  if @roster.inlude?(grade) == false
+    @roster[grade] = []
+  end
+  @roster[grade] << name
+end
+
+def grade(number)
+  @roster[number]
+end
+
+def sort
+  @roster.each do |grade, name|
+    @roster[grade] = name.sort
+  end
+end
 end
