85297a8da0dea5040020d3f2c2c32c214920e563
Devin Cooper <dcoope12@asu.edu>
2020-09-22 00:49:40 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -13,7 +13,7 @@ end
 def add_student(name, grade)
   @student_name = name
   @grade = grade
-  if @roster.inlude?(grade) == false
+  if @roster.include?(grade) == false
     @roster[grade] = []
   end
   @roster[grade] << name
